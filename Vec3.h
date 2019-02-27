fa988d073709621d2351c08d3b259862f5d86444
bdartnell <s5005667@bournemouth.ac.uk>
2019-02-27 13:24:41 +0000

    added default ctor

--- Vec3.h
+++ Vec3.h
@@ -5,7 +5,12 @@ class Vec3
 {
   public :
     Vec3()=default;
-    ~Vec3() noexcept =default;
+
+    Vec3(float _x, float _y, float _z) :
+        m_x{_x}, m_y{_y}, m_z{_z}
+    {}
+
+    Vec3() noexcept =default;
     Vec3(const Vec3 &)=default;
     Vec3 & operator=(const Vec3 &)=default;
     Vec3(Vec3 &&)=default;
